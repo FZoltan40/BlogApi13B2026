ba54afeed16eb365f94cf92d9adc16155fe1e929
FZoltan <farkaszoltan28@gmail.com>
2026-09-09 11:57:54 +0200

    Delete endpoint is ready to use.

--- BlogApi/Controllers/BloggerController.cs
+++ BlogApi/Controllers/BloggerController.cs
@@ -89,7 +89,21 @@ namespace BlogApi.Controllers
         [HttpDelete]
         public object DeleteBlogger(int id) 
         {
-            return null;
+            var connector = new MySqlConnection(ConnectionString);
+
+            connector.Open();
+
+            var sql = $"DELETE FROM blogger WHERE id = @id";
+
+            var cmd = new MySqlCommand(sql,connector);
+
+            cmd.Parameters.AddWithValue(@"id", id);
+
+            cmd.ExecuteNonQuery();
+
+            connector.Close();
+
+            return new { message = "Sikeres tölrés"};
         }
     }
 }
--- BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051a0b3d0479fd4ee2a56211b2d5ddba1563630d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+902a5918f5ebd20897f4a712d8109525d4bcc3f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
