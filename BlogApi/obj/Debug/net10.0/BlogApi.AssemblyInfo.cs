3fccac9ef756650a94e881c47d271b1f36065ea9
FZoltan <farkaszoltan28@gmail.com>
2026-09-16 11:58:53 +0200

    BloggerPostsNumber endpoint is defined on BlogPosConroller.

--- BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2addf1eafef40e91cd7be36c64d4486af47751")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1585dfd62e56e648ace023e668c597b5e5bf9ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
