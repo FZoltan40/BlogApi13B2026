ad2addf1eafef40e91cd7be36c64d4486af47751
FZoltan <farkaszoltan28@gmail.com>
2026-09-16 11:34:32 +0200

    BloggerOwnPost endpoint is defined on BlogPosConroller.

--- BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d498bbcbb31b2d73accf60f6787de591ae3b764d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64c23a09584e446cfe994c87c1024d4f4597b949")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
